63bdd76c1b96569eb11637bc860573a69fda2776
kenneth <kennethjayadiyu@gmail.com>
2023-05-05 17:11:07 +0700

    updated input into list

--- BinaryTreeNodes.java
+++ BinaryTreeNodes.java
@@ -1,8 +1,6 @@
 public class BinaryTreeNodes {
     public static void main(String[] args) {
-        Node root = new Node(5);
-        root.left = new Node(3);
-        root.right = new Node(7);
+        Node root = createTree(new int[]{4, 3, 5, 7, 6});
         System.out.println(countElements(root));
     }
 
@@ -17,6 +15,14 @@ public class BinaryTreeNodes {
         return num;
     }
 
+    public static Node createTree(int[] data){
+        Node root = new Node(data[0]);
+        for(int i = 1 ; i < data.length; i++){
+            root.insertNode(data[i]);
+        }
+        return root;
+    }
+
 }
 class Node {
     Node left;
@@ -27,5 +33,22 @@ class Node {
         this.data = data;
         left = null;
         right = null;
+    }
+
+    public void insertNode(int data){
+        if (data < this.data){
+            if (this.left == null){
+                this.left = new Node(data);
+            } else {
+                this.left.insertNode(data);
+            }
+        }
+        else if (data > this.data){
+            if (this.right == null){
+                this.right = new Node(data);
+            } else {
+                this.right.insertNode(data);
+            }
+        }
     }
 }
